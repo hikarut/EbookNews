0c6d47369c23ccfaf026b9479f7221a189ea7c53
HikaruTakahashi <hikaru@hikaru-takahashi-no-MacBook.local>
2016-01-30 18:04:19 +0900

    add Carthage,PagingMenuController

---
+++ EbookNews/PagingMenu1ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PagingMenu1ViewController.swift
+//  EbookNews
+//
+//  Created by HikaruTakahashi on 2016/01/30.
+//  Copyright © 2016年 HikaruTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class PagingMenu1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
